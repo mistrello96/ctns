e761c1cc3debe69aca8a5e3c63dfbfe9a0389463
Matteo Mistri <matteo.mistri1996@gmail.com>
2020-05-14 15:25:37 +0200

    Update version

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ import os
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
 setup(
   name = 'ctns',
   packages = find_packages(),
-  version = '0.2',
+  version = '0.2.1',
   scripts = ['bin/ctns', 'bin/ctns.bat','bin/ctns_run.py'],
   description = 'CTNS, Contact Tracing Network Simulator: a tool to simulate contact tracing in a population where a disease is spreading',
+  long_description_content_type = "text/markdown",
+  long_description = README,
+  license = 'GNU GPLv3',
   author = 'Matteo Mistri, Diego Miglio',
   author_email = 'matteo.mistri1996@gmail.com',
   install_requires = required,
